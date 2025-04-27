36e0da3d41ac5edcc34b8de9f5e958c637809225
Thiago Silva <thiago.rds@outlook.com.br>
2025-04-27 14:55:41 -0300

    feat(private-routes): created private routes and added docs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
+import { PrivateRoutes, PublicRoutes } from "./routes";
 import { FullPageLoader } from "./components";
 import { AuthContext } from "./context";
 import { useContext } from "react";
-import { PublicRoutes } from "./routes";
 
 
 
@@ -9,17 +9,13 @@ import { PublicRoutes } from "./routes";
 export const  App = () =>  {
   const {isAuthenticated, isLoading} = useContext(AuthContext);
   
-  console.log(isAuthenticated === true)
-
   return (
     <>
       {
         isLoading? (
           <FullPageLoader/>
         ) : isAuthenticated ? (
-          <div className="bg-gray-500 h-screen">
-            Primiero Commit   
-          </div>
+          <PrivateRoutes/>
         ) : (
           <PublicRoutes/>
         )
---
+++ src/routes/private.routes.tsx
@@ -0,0 +1,15 @@
+import { NotFoundPage } from "@/pages/not-found.page";
+import {Navigate, Route, Routes} from "react-router-dom";
+import { privateRoutes } from "./routes";
+
+export const PrivateRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/dashboard" replace />} />
+      {privateRoutes.map(({ path, element: Element }) => (
+        <Route key={path} path={path} element={<Element />} />
+      ))}
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
+};
