ba611e1d165bd55d4976c36bf985526cea7eb8a0
Thiago Silva <thiago.rds@outlook.com.br>
2025-04-29 18:09:17 -0300

    feat(docs): added doc data menu

--- src/components/menu/menu-desktop.components.tsx
+++ src/components/menu/menu-desktop.components.tsx
@@ -1,9 +1,9 @@
 import { Twirl as Hamburger } from 'hamburger-react';
 import LogoSymbol from "@/assets/symbol_logo.png";
-import { NavigationContext } from "@/context";
-import { useContext } from "react";
 import { NavMenu } from './nav-menu.components';
+import { NavigationContext } from "@/context";
 import {motion} from "framer-motion";
+import { useContext } from "react";
 
 export const MenuDesktop = () => {
   const {isSidebarOpen , toggleSideBar , resetPathsMenusAndNavigateDashboard } = useContext(NavigationContext);
@@ -42,52 +42,3 @@ export const MenuDesktop = () => {
     </motion.aside>
   );
 };
-
-
-
-
-{/* <motion.aside
-layout
-initial={false}
-animate={{ width: true ? 256 : 80 }}
-transition={{ duration: 0.4, ease: "easeInOut" }}
-//   onTransitionEnd={handleSidebarTransitionEnd}
-className="hidden lg:flex h-screen bg-white-default border-r flex-col z-50 absolute left-0 overflow-hidden shadow-[-1px_4px_3px_4px_var(--color-shadow)]"
->
-{/* Topo - Logo e Botão */}
-{/* <motion.div layout className="flex items-center justify-between p-4">
-  <div
-    className="flex items-center cursor-pointer"
-  //   onClick={resetPathsMenusAndNavigateDashboard}
-  >
-    <img src={SymbolLogo} alt="Logo" className="w-10 h-10" />
-    {true && (
-      <motion.span
-        layout
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        transition={{ delay: 0.1 }}
-        className="ml-2 font-bold text-lg whitespace-nowrap"
-      >
-        Cadastro RFK
-      </motion.span>
-    )}
-  </div>
-
-  <Button
-  //   onClick={toggleOpenMenu}
-    text=""
-    variant="ghost"
-    className="rounded hover:bg-gray-100 absolute right-2"
-    iconInText={true ? CloseMenuIcon : OpenMenuIcon}
-    styleIcon={{ color: true ? "var(--color-strong)" : "var(--color-neutral)", size: 20 }}
-  />
-</motion.div>
-
-{/* Linha divisória */}
-{/* <div className="border-b border-border/70" /> */}
-
-{/* Menu de Navegação */}
-{/* <NavMenu /> */}
-// </motion.aside> */} */}
