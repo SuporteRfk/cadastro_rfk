f03c5c97517404e4b5648d4d3c8f6c3b86928b07
Thiago Silva <thiago.rds@outlook.com.br>
2025-05-06 11:16:12 -0300

    feat(feature-insumo): created base structure and interface

---
+++ src/features/insumos/page/register-insumo.page.tsx
@@ -0,0 +1,12 @@
+import { FormLayout, PageLayout } from "@/components";
+import {Droplet as InsumoIcon} from "lucide-react";
+
+export const RegisterInsumo = () => {
+    return(
+        <PageLayout>
+            <FormLayout titleForm="Insumos" iconForm={InsumoIcon}>
+         
+            </FormLayout>
+        </PageLayout>
+    );
+};
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,10 +4,11 @@ import { RegisterPACopacker } from "@/features/pa-copacker/page/register-pa-copa
 import { RegisterPAUnitary } from "@/features/pa-unitary/page/register-pa-unitary.page";
 import { RegisterPABurden } from "@/features/pa-burden/page/register-pa-burden.page";
 import { RegisterSupplier } from "@/features/suppliers/page/register-supplier.page";
+import { RegisterPAThird } from "@/features/pa-third/page/register-pa-third.page";
 import { RegisterClientPage } from "@/features/client/page/register-client.page";
+import { RegisterInsumo } from "@/features/insumos/page/register-insumo.page";
 import { DashboardPage } from "@/features/dashboard/page/dashboard.page";
 import { LoginPage } from "@/features/login/page/login.page";
-import { RegisterPAThird } from "@/features/pa-third/page/register-pa-third.page";
 
 
 // Arquivo para organiar as rotas do sistema
@@ -17,14 +18,14 @@ export const privateRoutes = [
     {path: "/cadastro/cliente", element: RegisterClientPage},
     {path: "/cadastro/condicao-pagamento", element: RegisterPaymentCondition},
     {path: "/cadastro/fornecedores", element: RegisterSupplier},
-    {path: "/cadastro/insumos", element: "teste"},
+    {path: "/cadastro/insumos", element: RegisterInsumo},
     {path: "/cadastro/pa-copacker", element: RegisterPACopacker},
     {path: "/cadastro/pa-fardo", element: RegisterPABurden},
     {path: "/cadastro/pa-terceiro", element: RegisterPAThird},
     {path: "/cadastro/pa-unitario", element: RegisterPAUnitary},
     {path: "/cadastro/produtos-indiretos", element: "teste"},
     {path: "/cadastro/unidade-medida", element: RegisterUnitMeasure},
-    {path: "/solicitar-alteracao", element: RegisterClientPage},
+    {path: "/solicitar-alteracao", element: "teste-altercao"},
     {path: "/solicitacoes/pendentes", element: "teste"},
     {path: "/solicitacoes/em-revisao", element: "teste"},
     {path: "/solicitacoes/negadas", element: "teste"},
