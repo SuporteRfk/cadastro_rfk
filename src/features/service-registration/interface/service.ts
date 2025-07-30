8f92120c98ba66b6e64ccf78fa0c1afab673dcee
Thiago Silva <thiago.rds@outlook.com.br>
2025-07-30 14:01:50 -0300

    feat(form-service): created interface

---
+++ src/features/service-registration/interface/service.ts
@@ -0,0 +1,20 @@
+import { FamilyCodeService, GroupCodeService, TypeCodeService } from "./service-enum";
+
+
+export interface IServiceRegistration {
+    id: number;
+    criado_em: string;
+    nome_solicitante:string;
+    email: string;
+    id_usr_keycloak: string;
+    whatsapp: string;
+    codigo_familia: FamilyCodeService;
+    codigo_grupo: GroupCodeService;
+    nome_cadastro: string;
+    tipo: TypeCodeService;
+    descricao: string;
+    codigo_servico:string;
+};
+
+
+export interface IServiceRegister extends Omit <IServiceRegistration,'id'>{};
