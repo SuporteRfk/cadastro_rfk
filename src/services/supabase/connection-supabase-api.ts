cef0436217e2e41d172e9cbafb8b6c9e3455d438
Thiago Silva <thiago.rds@outlook.com.br>
2025-07-31 23:16:33 -0300

    fix: delete consoles logs of code

--- src/services/supabase/connection-supabase-api.ts
+++ src/services/supabase/connection-supabase-api.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const schemaDb = import.meta.env.VITE_DB_SCHEMA || "public";
-console.log(schemaDb)
+
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 export const supabaseApi = createClient(supabaseUrl,supabaseKey, {db: {schema: schemaDb}});
